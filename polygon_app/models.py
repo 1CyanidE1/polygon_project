3f0a7a0363618fa72f6409c8b511ccc5a5af7655
1Cyanide1 <vanoni911@gmail.com>
2024-11-05 22:05:32 +0300

    Polygon models

--- polygon_app/models.py
+++ polygon_app/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Polygon
 
-# Create your models here.
+
+class PolygonModel(models.Model):
+    name = models.CharField(max_length=200)
+    polygon = models.PolygonField(srid=4326)
+    crosses_antimeridian = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        coords = self.polygon.coords[0]
+        for i in range(len(coords) - 1):
+            lon1 = coords[i][0]
+            lon2 = coords[i + 1][0]
+
+            if abs(lon1 - lon2) > 180:
+                self.crosses_antimeridian = True
+
+                new_coords = []
+                for coord in coords:
+                    lon = coord[0]
+                    lat = coord[1]
+                    if lon > 180:
+                        lon = lon - 360
+                    new_coords.append((lon, lat))
+
+                self.polygon = Polygon(new_coords)
+                break
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
