420caa736cf767e82979cf8601d26a9cae3e8255
1Cyanide1 <vanoni911@gmail.com>
2024-11-06 12:58:27 +0300

    urls.py

--- polygon_project/urls.py
+++ polygon_project/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from polygon_app import views
+
+router = DefaultRouter()
+router.register(r'polygons', views.PolygonViewSet)
 
 urlpatterns = [
+    path('', views.PolygonView.as_view(), name='index'),
+    path('api/', include(router.urls)),
+    path('api/save-polygon/', views.save_polygon, name='save_polygon'),
     path('admin/', admin.site.urls),
 ]
